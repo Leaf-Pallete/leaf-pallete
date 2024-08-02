59f6a1f49bda39ac3702a8460ebf91c3ae626151
cardrocha <cardrocharj@gmail.com>
2024-08-01 21:20:48 -0300

    feat: Applied responsive layout and style to NavBar, Sidebar, and Header

--- components/header.tsx
+++ components/header.tsx
@@ -1,24 +1,25 @@
 import * as I from "@radix-ui/react-icons";
 
-import { Input } from "@/components/ui/input"
+import { Input } from "@/components/ui/input";
 import NavBar from "./navBar";
 import Sidebar from "./siderBar";
 
-
-
 const Header = () => {
+	
 	return (
-		<header className="flex items-start gap-8 md:gap-8 justify-between px-2 md:pr-8 pt-8">
+		<header className="flex items-center md:items-start gap-1 md:gap-8 justify-between p-6 md:px-4 md:py-8">
 			<Sidebar />
-			<div className=" relative flex justify-between w-full">
+			<div className="relative flex gap-4 justify-between md:w-full">
 				<Input
-					className="border border-gray-300 pl-12 outline-gray-300 w-full md:w-[528px] h-14 rounded-2xl"
+					id="placeholder"
+					className="border opacity-0 md:opacity-100 focus:opacity-50 focus:placeholder-transparent md:focus:placeholder-muted-foreground border-border outline-border md:text-base pl-2 md:pl-12 w-[130px] md:w-[528px] h-10 md:h-14 rounded-2xl cursor-pointer"
 					type="text"
-					placeholder="Pesquise pelo conteúdo aqui..."
-				/>
-				<I.MagnifyingGlassIcon className="absolute top-4 h-6 w-6 left-5 md:left-4" />
-				<NavBar />
+					placeholder="Pesquise pelo conteúdo aqui..." 		/>
+				<label htmlFor="placeholder">
+					<I.MagnifyingGlassIcon className="text-muted-foreground absolute top-1 md:top-4 h-7 w-7 left-24 md:left-4 cursor-pointer" />
+				</label>
 			</div>
+			<NavBar />
 		</header>
 	);
 };
