b39ca2def3130c7792b437d0a7aadc642bb36c8c
douglas sales <douglas-8545@hotmail.com>
2024-08-23 13:37:34 -0300

    ref: changed component for using image added style button with shadcn and changed colors in hard-code

--- components/Home/CardDesignStyles/CardDesignStyles.tsx
+++ components/Home/CardDesignStyles/CardDesignStyles.tsx
@@ -1,36 +1,44 @@
+import { buttonVariants } from "@/components/ui/button";
+import { cn } from "@/lib/utils";
 import { FigmaLogoIcon } from "@radix-ui/react-icons";
+import Image from "next/image";
 import Link from "next/link";
-import { useState } from "react";
-import { FirstCard } from "./FirstCard";
-import { SecondCard } from "./SecondCard";
 
 export const CardDesignStyles = () => {
-	const [applyEffect, setApplyEffect] = useState<boolean>(false);
-
 	return (
-		<div className="relative w-[325px] sm:w-[575px] border rounded-t-xl rounded-b-lg overflow-hidden">
+		<div className="relative w-[325px] sm:w-[575px] rounded-t-lg sm:rounded-t-xl rounded-b-lg overflow-hidden">
 			<div className="absolute inset-0 bg-gradient-to-b from-[#16A249] to-[#F8C8FF] opacity-20" />
-			<div
-				className="relative z-10 w-full h-[288px] sm:h-[436px] flex flex-col justify-end items-center rounded-t-xl cursor-pointer"
-				onMouseEnter={() => setApplyEffect(true)}
-				onMouseLeave={() => setApplyEffect(false)}
-			>
-				<div className="w-[284px] h-[265px] sm:w-[503px] sm:h-[408px] flex flex-col bg-white rounded-t-xl border-t-2 border-r-2 border-l-2 border-[#E4E4E7]">
+			<div className="relative z-10 w-full h-[260px] sm:h-[436px] flex flex-col justify-end items-center rounded-t-lg sm:rounded-t-xl cursor-pointer">
+				<div className="w-[284px] h-[235px] sm:w-[503px] sm:h-[392px] flex flex-col bg-card rounded-t-lg sm:rounded-t-xl">
 					<div>
 						<p className="text-card-foreground font-semibold text-xs sm:text-xl pt-10 pl-4 sm:pl-6">
 							Design Metrics
 						</p>
 					</div>
-					<div className=" pl-4 sm:pl-5 flex flex-col h-full relative pt-[10px] sm:pt-[18px]">
-						<FirstCard applyEffect={applyEffect} />
-						<SecondCard applyEffect={applyEffect} />
+					<div className="hidden pl-4 sm:pl-5 sm:flex flex-col pt-[10px] sm:pt-[48px]">
+						<Image
+							src="/image-card-desing-styles.png"
+							alt=""
+							title=""
+							width={457}
+							height={276}
+						/>
+					</div>
+					<div className="flex justify-center sm:hidden pt-[10px]">
+						<Image
+							src="/image-card-desing-styles.png"
+							alt="Imagem de exibição do card"
+							title="Imagem de exibição do card"
+							width={254}
+							height={169}
+						/>
 					</div>
 				</div>
 			</div>
-			<div className="relative z-10 w-full h-[101px] flex items-center px-4 sm:px-[30px] bg-white border-2 border-[#E4E4E7] rounded-md ">
+			<div className="relative z-10 w-full h-[101px] flex items-center px-4 sm:px-[30px] bg-card border-2 border-border rounded-md ">
 				<div className="w-full flex flex-row justify-between">
 					<div className="flex flex-row items-center gap-[6px] sm:gap-3">
-						<div className="w-[35px] h-[35px] sm:w-[61px] sm:h-[61px] flex justify-center items-center bg-[#16A24933] rounded-full">
+						<div className=" hidden w-[61px] h-[61px] sm:flex justify-center items-center bg-primary/20 rounded-full">
 							<FigmaLogoIcon className="w-6 h-6 text-primary" />
 						</div>
 						<div>
@@ -48,11 +56,9 @@ export const CardDesignStyles = () => {
 					<div className="flex items-center">
 						<Link
 							href="#"
-							className="bg-primary px-[14px] py-[6px] sm:px-[26px] sm:py-[9px] rounded-md"
+							className={`${cn(buttonVariants({ variant: "default" }))} sm:px-[26px] font-medium`}
 						>
-							<p className=" text-[8px] sm:text-base font-medium text-white">
-								Learn more
-							</p>
+							Learn more
 						</Link>
 					</div>
 				</div>
