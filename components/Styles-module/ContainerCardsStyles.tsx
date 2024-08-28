b1ea0f403bdbc913d96917e2d8bc833031b3c62d
douglas sales <douglas-8545@hotmail.com>
2024-08-28 13:49:12 -0300

    ref: changed class in element for adaptation mobile

--- components/Styles-module/ContainerCardsStyles.tsx
+++ components/Styles-module/ContainerCardsStyles.tsx
@@ -4,7 +4,7 @@ import { CardsStylesModules } from "./CardStylesModules";
 
 export const ContainerCardsStyles = () => {
 	return (
-		<div className="w-full flex flex-wrap justify-between gap-6 pt-8">
+		<div className="w-full flex justify-center flex-wrap lg:justify-between gap-6 pt-8">
 			{infoCardsStyelModule.map((card: CardProps) => (
 				<CardsStylesModules key={card.id} item={{ ...card }} />
 			))}
