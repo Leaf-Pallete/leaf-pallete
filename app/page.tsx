45f5a5ca394ae60e18c5affdbba933da87acfa2d
Ricardo Magalhães <ricardogleiss@hotmail.com>
2024-09-01 10:57:16 -0300

    feat: create home page

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,23 @@
+import { CardDesignStyles } from "@/components/Home/CardDesignStyles/CardDesignStyles";
+import { CardUiComponents } from "@/components/Home/CardUiComponents/CardUiComponents";
+import Hero from "@/components/Home/HeroSection/Hero";
+import LastUpdates from "@/components/Home/last-updates";
+import Resources from "@/components/Home/resources";
 import Header from "@/components/shared/Header";
 
 export default function Home() {
 	return (
-		<main>
+		<>
 			<Header />
-		</main>
+			<Hero />
+			<main className="py-20 max-w-[90%] mx-auto">
+				<div className="flex max-w-screen-xl gap-8 w-full mx-auto justify-between items-center flex-col lg:flex-row">
+					<CardUiComponents />
+					<CardDesignStyles />
+				</div>
+				<LastUpdates />
+				<Resources />
+			</main>
+		</>
 	);
 }
