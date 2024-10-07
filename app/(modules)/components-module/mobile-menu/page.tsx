c9a346413c8a916aa40617153f6eb76d6b4cde67
Ricardo Magalhães <ricardogleiss@hotmail.com>
2024-10-07 17:17:31 -0300

    style: code reformat

--- app/(modules)/components-module/mobile-menu/page.tsx
+++ app/(modules)/components-module/mobile-menu/page.tsx
@@ -1,20 +1,20 @@
 import React from "react";
 import {
-	Hero,
-	UseCases,
-	Anatomy,
-	BestPractices,
+  Hero,
+  UseCases,
+  Anatomy,
+  BestPractices,
 } from "@/components/components-module/mobile-menu";
 
 const MobileMenu = () => {
-	return (
-		<div className="flex flex-col gap-8">
-			<Hero />
-			<UseCases />
-			<Anatomy />
-			<BestPractices />
-		</div>
-	);
+  return (
+    <div className="flex flex-col gap-8">
+      <Hero />
+      <UseCases />
+      <Anatomy />
+      <BestPractices />
+    </div>
+  );
 };
 
 export default MobileMenu;
--- components/components-module/mobile-menu/hero.tsx
+++ components/components-module/mobile-menu/hero.tsx
@@ -1,48 +1,48 @@
 import {
-	heroDesktop,
-	heroDesktopDark,
-	heroMobile,
-	heroMobileDark,
+  heroDesktop,
+  heroDesktopDark,
+  heroMobile,
+  heroMobileDark,
 } from "@/public/components-module/mobile-menu/images";
 import Image from "next/image";
 import React from "react";
 
 const Hero = () => {
-	return (
-		<section className="flex flex-col gap-4">
-			<h1 className="text-4xl sm:text-6xl font-semibold">Mobile Menu</h1>
-			<p className="">
-				A mobile menu is a navigation solution designed specifically for mobile
-				devices, making it easy to access different parts of a website or app in
-				a compact and efficient manner. Tailored for smaller screens, it
-				typically features various styles, such as horizontal navigation bars,
-				expandable menus, and floating buttons, ensuring users can explore
-				content effortlessly and with an optimized experience.
-			</p>
-			<div className="">
-				<Image
-					src={heroMobile}
-					alt=""
-					className="block dark:hidden w-full mx-auto max-w-[491px] sm:hidden"
-				/>
-				<Image
-					src={heroDesktop}
-					alt=""
-					className="hidden sm:block mx-auto w-full max-w-[1047px] dark:hidden"
-				/>
-				<Image
-					src={heroMobileDark}
-					alt=""
-					className="hidden dark:block w-full mx-auto max-w-[491px] sm:dark:hidden"
-				/>
-				<Image
-					src={heroDesktopDark}
-					alt=""
-					className="hidden sm:dark:block mx-auto w-full max-w-[1047px]"
-				/>
-			</div>
-		</section>
-	);
+  return (
+    <section className="flex flex-col gap-4">
+      <h1 className="text-4xl sm:text-6xl font-semibold">Mobile Menu</h1>
+      <p className="">
+        A mobile menu is a navigation solution designed specifically for mobile
+        devices, making it easy to access different parts of a website or app in
+        a compact and efficient manner. Tailored for smaller screens, it
+        typically features various styles, such as horizontal navigation bars,
+        expandable menus, and floating buttons, ensuring users can explore
+        content effortlessly and with an optimized experience.
+      </p>
+      <div className="">
+        <Image
+          src={heroMobile}
+          alt=""
+          className="block dark:hidden w-full mx-auto max-w-[491px] sm:hidden"
+        />
+        <Image
+          src={heroDesktop}
+          alt=""
+          className="hidden sm:block mx-auto w-full max-w-[1047px] dark:hidden"
+        />
+        <Image
+          src={heroMobileDark}
+          alt=""
+          className="hidden dark:block w-full mx-auto max-w-[491px] sm:dark:hidden"
+        />
+        <Image
+          src={heroDesktopDark}
+          alt=""
+          className="hidden sm:dark:block mx-auto w-full max-w-[1047px]"
+        />
+      </div>
+    </section>
+  );
 };
 
 export default Hero;
