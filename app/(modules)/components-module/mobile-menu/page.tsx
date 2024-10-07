bb5a2c0decd3ff49507d65fbe6add9f4b5f606f8
Ricardo Magalhães <ricardogleiss@hotmail.com>
2024-10-06 23:49:06 -0300

    feat: mobile menu section

---
+++ app/(modules)/components-module/mobile-menu/page.tsx
@@ -0,0 +1,20 @@
+import React from "react";
+import {
+	Hero,
+	UseCases,
+	Anatomy,
+	BestPractices,
+} from "@/components/components-module/mobile-menu";
+
+const MobileMenu = () => {
+	return (
+		<div className="flex flex-col gap-8">
+			<Hero />
+			<UseCases />
+			<Anatomy />
+			<BestPractices />
+		</div>
+	);
+};
+
+export default MobileMenu;
